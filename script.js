df09676a5256e7a82406bbf9521db7f25d69a9e6
zicojr <fegyisaac160@gmail.com>
2023-09-09 23:43:05 +0100

    done fetching api

--- script.js
+++ script.js
@@ -5,27 +5,13 @@ const button = document.getElementById("button");
 URL_APIKEY = "https://api.adviceslip.com/advice";
 
 const clickHandler = async () => {
-    const res = await fetch('https://api.adviceslip.com/advice');
-    const data = await res.json();
-    console.log(data);
-}
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
 
-// const clickHandler = async () => {
-//   try {
-//     fetch("https://wordsapiv1.p.mashape.com/words/simple")
-//       .then((res) => res.json())
-//       .then((data) => console.log(data));
-//   } catch (error) {
-//     console.log("we got a big problem dickface");
-//   }
-// };
-
-// let clickHandler = () => {
-//   fetch(URL_APIKEY)
-//     .then((data) => data.json())
-//     .then((item) => {
-//       console.log(item);
-//     });
-// };
+  adviceSlip.innerHTML = data.slip.advice;
+  adviceID.innerHTML = data.slip.id;
+};
 
 button.addEventListener("click", clickHandler);
+
+window.addEventListener("load", clickHandler);
